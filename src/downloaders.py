9fa78e74fc4c70c3629c7d294c733bee4d233da5
kun <qbetterkk@gmail.com>
2024-04-22 22:34:02 -0400

    add download function; add row[_source] during download step, so that we can use it at preparing step

--- src/downloaders.py
+++ src/downloaders.py
@@ -14,7 +14,7 @@ import chardet
 import requests
 import pandas as pd
 
-from datasets import load_dataset, list_datasets, Dataset
+from datasets import load_dataset, list_datasets, Dataset, concatenate_datasets
 
 # `HfFileSystem` requires the latest version of `huggingface_hub`
 from huggingface_hub import HfFileSystem, hf_hub_url, hf_hub_download, login
@@ -1216,4 +1216,22 @@ def download_opengpt_healthcare(accepted_filter_ids):
 def download_conifer(accepted_filter_ids):
     dset = huggingface_download("ConiferLM/Conifer", split="train_sft")
     return dset
+
+
+def download_dialogstudio(accepted_filter_ids):
+    dsets = []
+    for data_name in ['chitchat-dataset', 'ConvAI2', 'AntiScam', 'Empathetic', 'HH-RLHF', 'PLACES3.5', 'Prosocial', 'SODA', 'ShareGPT', 'CompWebQ', 
+                        'CoQA', 'CoSQL', 'DART', 'FeTaQA', 'GrailQA', 'HybridQA', 'MTOP', 'MultiModalQA', 'SParC', 'Spider', 'SQA', 'ToTTo', 'WebQSP', 
+                        'WikiSQL', 'WikiTQ', 'wizard_of_internet', 'wizard_of_wikipedia', 'AMI', 'CRD3', 'DialogSum', 'ECTSum', 'ICSI', 'MediaSum', 
+                        'QMSum', 'SAMSum', 'TweetSumm', 'ConvoSumm', 'SummScreen_ForeverDreaming', 'SummScreen_TVMegaSite', 'ATIS', 'ATIS-NER', 
+                        'BANKING77', 'BANKING77-OOS', 'CLINC-Single-Domain-OOS-banking', 'CLINC-Single-Domain-OOS-credit_cards', 'CLINC150', 'DSTC8-SGD', 
+                        'HWU64', 'MIT-Movie', 'MIT-Restaurant', 'RESTAURANTS8K', 'SNIPS', 'SNIPS-NER', 'TOP', 'TOP-NER', 'ABCD', 'AirDialogue', 
+                        'BiTOD', 'CaSiNo', 'CraigslistBargains', 'Disambiguation', 'DSTC2-Clean', 'FRAMES', 'GECOR', 'HDSA-Dialog', 'KETOD', 'KVRET', 
+                        'MetaLWOZ', 'MS-DC', 'MuDoCo', 'MulDoGO', 'MultiWOZ_2.1', 'MULTIWOZ2_2', 'SGD', 'SimJointGEN', 'SimJointMovie', 'SimJointRestaurant', 
+                        'STAR', 'Taskmaster1', 'Taskmaster2', 'Taskmaster3', 'WOZ2_0', 'Redial', 'DuRecDial-2.0', 'OpenDialKG', 'SalesBot']:
+        if f"ds-{data_name}" in accepted_filter_ids:
+            dset = huggingface_download("Salesforce/dialogstudio", data_name, split="train")
+            dset = annotate_source(dset, f"ds-{data_name}")
+            dsets.extend(dset)
+    return dsets
 
