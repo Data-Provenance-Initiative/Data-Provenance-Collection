23786071aab485090f8eba0fe161050ba8589eeb
William Brannon <wbrannon@media.mit.edu>
2024-05-03 01:29:58 +0000

    split up tables

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -7,3 +7,41 @@ def count_unique_with_none(s):
             all_vals += lst
     
     return len(set(all_vals))
+
+
+def color_license_classes(s):
+    ret = []
+
+    if 'All' in s:
+        ret += [r'\CommercialDataCircle']
+    else:
+        ret += [r'\TransparentCircle']
+    
+    if 'Unspecified' in s or len(s) == 0:
+        ret += [r'\UnspecifiedDataCircle']
+    else:
+        ret += [r'\TransparentCircle']
+    
+    if 'Acad' in s or 'NC' in s:
+        ret += [r'\NCDataCircle']
+    else:
+        ret += [r'\TransparentCircle']
+    
+    return ' '.join(ret)
+
+
+FORMATS_MAP = {
+    'Zero-shot': 'ZS',
+    'zero-shot': 'ZS',
+    'Few-shot': 'FS',
+
+    'Single-turn Dialog': 'SD',
+    'Multi-turn Dialog': 'MD',
+    
+    'Chain-of-Thought': 'CT',
+    'Program of Thoughts': 'PT',
+    'Program-of-Thought': 'PT',
+    
+    'Response Ranking': 'RR',
+    'Evaluation': 'EV',
+}
