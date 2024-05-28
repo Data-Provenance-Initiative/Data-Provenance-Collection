bfa6d6c6065d12f6d2e0882d48035a41dbd095bb
Nikhil Singh <nikhilsinghmus@gmail.com>
2024-05-28 15:35:18 -0400

    Add initial draft multimodal analysis notebook. Update constants and license handling for multimodal processing.

--- src/helpers/constants.py
+++ src/helpers/constants.py
@@ -3,6 +3,7 @@ import os
 
 LICENSE_CONSTANTS_FP = "license_classes.json"
 CUSTOM_LICENSE_CONSTANTS_FP = "custom_license_classes.json"
+LICENSE_PARAPHRASES_FP = "license_paraphrases.json"
 LANGUAGE_CONSTANTS_FP = "language_families.json"
 TASK_CONSTANTS_FP = "task_groups.json"
 TOPIC_CONSTANTS_FP = "topic_groups.json"
@@ -11,5 +12,6 @@ CREATOR_CONSTANTS_FP = "creator_groups.json"
 CREATOR_COUNTRY_CONSTANTS_FP = "creator_groups_by_country.json"
 FORMATS_CONSTANTS_FP = "data_formats.json"
 DOMAINS_CONSTANTS_FP = "domain_groups.json"
+DOMAIN_TYPES_CONSTANTS_FP = "domain_types.json"
 
 LICENSE_USE_TYPES = ['commercial', 'unspecified', 'non-commercial', 'academic-only']
